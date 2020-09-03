5d315bbedf9d8d55bea64999fc4cfe372fddb456
drogan120 <alimahmudin.hp@gmail.com>
2020-09-03 20:57:01 +0700

    adding remove functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", removeTodo);
 
 function addTodo(e) {
   e.preventDefault();
@@ -11,18 +12,27 @@ function addTodo(e) {
   todoDiv.classList.add("todo");
 
   const newTodo = document.createElement("li");
-  newTodo.innerHTML = "hey";
+  newTodo.innerHTML = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
   const completedButton = document.createElement("button");
-  completedButton.innerHTML = "Completed";
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("completed-btn");
   todoDiv.appendChild(completedButton);
 
   const trashButton = document.createElement("button");
-  trashButton.innerHTML = "Delete";
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
+  todoInput.value = "";
+}
+
+function removeTodo(e) {
+  const item = e.target;
+  if (item.classList[0] == "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
 }
